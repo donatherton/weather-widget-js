42f79d3eb05a1d0fde5c268fa6ed846252c82a25
Don Atherton <don@donatherton.co.uk>
2025-09-02 00:20:45 +0100

    Beginning major refactor

---
+++ defaults.js
@@ -0,0 +1,6 @@
+const	defaults = {
+	"appid": "7f8871108ffeac097a03c40598d0232f",
+	"place": "Falmouth",
+	"lat":  "50.15562",
+	"lon":  "-5.07071"
+}
---
+++ serviceWorker.js
@@ -0,0 +1,51 @@
+const ASSET_CACHE = 'weather-v1';
+const assets = [
+  "/",
+  "/index.html",
+  "/hourly.html",
+  "/5-days.html",
+  "/leaflet.js",
+  "/leaflet.css",
+  "/radar.html",
+  "/PNG/01d.png",
+  "/PNG/01n.png",
+  "/PNG/02d.png",
+  "/PNG/02n.png",
+  "/PNG/03d.png",
+  "/PNG/03n.png",
+  "/PNG/04d.png",
+  "/PNG/04n.png",
+  "/PNG/09d.png",
+  "/PNG/09n.png",
+  "/PNG/10d.png",
+  "/PNG/10n.png",
+  "/PNG/11d.png",
+  "/PNG/11n.png",
+  "/PNG/13d.png",
+  "/PNG/13n.png",
+  "/PNG/50d.png",
+  "/PNG/50n.png",
+  "/css/owfont-regular.css",
+  "/css/owfont-regular.min.css",
+  "/fonts/owfont-regular.eot",
+  "/fonts/owfont-regular.otf",
+  "/fonts/owfont-regular.svg",
+  "/fonts/owfont-regular.ttf",
+  "/fonts/owfont-regular.woff",
+]
+
+self.addEventListener('install', (event) => {
+  console.log('ServiceWorker installing');
+  event.waitUntil(
+    caches.open(ASSET_CACHE)
+    .then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
